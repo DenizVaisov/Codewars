644d18bae88e684eab01ec52369c207a46fe3b60
Deniz <vaisov96@mail.ru>
2020-08-02 21:02:24 +0300

    Code wars

---
+++ Codewars/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class Kata
+    {
+        private static Tuple<char, int> tuple(char character, int count) {
+            return new Tuple<char, int>(character, count);
+        }
+        public static List<Tuple<char, int>> OrderedCount(string input) {
+            List<Tuple<char, int>> countedChars = new List<Tuple<char, int>>();
+            int counter = 0;
+            char prevChar = '\\'; 
+            foreach (var item in input) 
+            {
+                foreach (var item1 in input) 
+                    if(item1 != prevChar && item1 == item)
+                        counter++;
+              
+                if (counter != 0 && !countedChars.Contains(new Tuple<char, int>(item, counter))) 
+                    countedChars.Add(tuple(item, counter)); 
+
+                counter = 0;
+                prevChar = item;
+            }
+          
+            return countedChars;
+        }
+    }
+}
---
+++ TwoToOne/TwoToOne.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TwoToOne
+{
+    public class TwoToOne
+    {
+        public static string Longest (string s1, string s2)
+        {
+            string[] strArray = {s1, s2};
+//            IEnumerable<string> words = strArray.OrderBy(y => s1.Count()).ThenBy(z => s2.Count());
+//            IEnumerable<string> words = strArray.Where(z => z.Length == (strArray.Max(n => n.Length))).Max().Distinct();
+            Array.Sort(strArray, (x,y) => String.CompareOrdinal(s1, s2));
+            
+            return null;
+        }
+    }
+}
